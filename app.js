9de86f48cf16bb056b0aa1880ba962043ca783a0
PavelKovalchuk <kovpavanat@gmail.com>
2019-02-17 15:10:08 +0200

    express - installed

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
 const http = require('http');
+const express = require('express');
 
-const routes = require('./routes');
+const app = express();
 
-const server = http.createServer(routes.handler);
+// function "use" for every upcoming request
+app.use((req, res, next) => {
+  console.log('In the middleware');
+
+  // next() -> go to the next middleware
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log('In another middleware');
+  console.log('---------------------');
+
+  res.send('<h1>Hello</h1>');
+});
+
+const server = http.createServer(app);
 
 server.listen(3000);
