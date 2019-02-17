9c3ba460c8c435c8ddce288cbb5c0bd0da6189cd
PavelKovalchuk <kovpavanat@gmail.com>
2019-02-17 23:09:39 +0200

    express - body parser

--- app.js
+++ app.js
@@ -1,23 +1,36 @@
-const http = require('http');
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-// function "use" for every upcoming request
-app.use((req, res, next) => {
-  console.log('In the middleware');
+// 3-party package for encoding data in the body
+// Middleware pattern
+app.use(bodyParser.urlencoded());
 
-  // next() -> go to the next middleware
-  next();
+app.use('/add-product', (req, res, next) => {
+  console.log('In add-product middleware');
+  res.send('' +
+    '<div>' +
+    '<h1>add product</h1>' +
+    '<form action="product" method="post">' +
+    '<input type="text" name="title" />' +
+    '<button type="submit">Add prodcut</button>' +
+    '</form>'+
+    '</div>'
+  );
 });
 
-app.use((req, res, next) => {
+app.use('/product', (req, res, next) => {
+  console.log('product req.body: ', req.body);
+  res.redirect('/');
+});
+
+// function "use" for every upcoming request
+app.use('/', (req, res, next) => {
   console.log('In another middleware');
   console.log('---------------------');
 
   res.send('<h1>Hello</h1>');
 });
 
-const server = http.createServer(app);
-
-server.listen(3000);
+app.listen(3000);
