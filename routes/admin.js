ca8347db9ae43874d750d244e12a8bf43e73b3ad
PavelKovalchuk <kovpavanat@gmail.com>
2019-02-19 23:12:15 +0200

    express - prettify

--- routes/admin.js
+++ routes/admin.js
@@ -1,20 +1,20 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 const router = express.Router();
-const rootDir = require('../util/path');
+const rootDir = require("../util/path");
 
 // PATH /admin/add-product => GET
-router.get('/add-product', (req, res, next) => {
-  console.log('In add-product middleware');
-  console.log('rootDir', rootDir);
-  res.sendFile(path.join(rootDir ,'views' , 'add-product.html'));
+router.get("/add-product", (req, res, next) => {
+  console.log("In add-product middleware");
+  console.log("rootDir", rootDir);
+  res.sendFile(path.join(rootDir, "views", "add-product.html"));
 });
 
 // FIlter only GET-request / POST-request
 // PATH /admin/add-product => POST
-router.post('/add-product', (req, res, next) => {
-  console.log('product req.body: ', req.body);
-  res.redirect('/');
+router.post("/add-product", (req, res, next) => {
+  console.log("product req.body: ", req.body);
+  res.redirect("/");
 });
 
 module.exports = router;
--- routes/shop.js
+++ routes/shop.js
@@ -1,15 +1,15 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 const router = express.Router();
-const rootDir = require('../util/path');
+const rootDir = require("../util/path");
 
 // function "use" for every upcoming request
-router.get('/', (req, res, next) => {
-  console.log('In common middleware');
-  console.log('---------------------');
+router.get("/", (req, res, next) => {
+  console.log("In common middleware");
+  console.log("---------------------");
 
   // __dirname - absolute path to this file
-  res.sendFile(path.join(rootDir ,'views' , 'shop.html'));
+  res.sendFile(path.join(rootDir, "views", "shop.html"));
 });
 
 module.exports = router;
