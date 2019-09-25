3e53bb2be4e1fe6057ddee49e1f6a201a23d83ef
PavelKovalchuk <pavel.kovalchuk@dataart.com>
2019-09-25 13:19:10 +0300

    106/ storing product data

--- controllers/admin.js
+++ controllers/admin.js
@@ -12,7 +12,12 @@ exports.getAddProduct = (req, res, next) => {
 };
 
 exports.postAddProduct = (req, res, next) => {
-  const product = new Product(req.body.title);
+  const title = req.body.title;
+  const imageUrl = req.body.imageUrl;
+  const price = req.body.price;
+  const description = req.body.description;
+
+  const product = new Product(title, imageUrl, price, description);
   product.save();
   console.log("product req.body: ", req.body);
   res.redirect("/");
@@ -20,6 +25,7 @@ exports.postAddProduct = (req, res, next) => {
 
 exports.getProducts = (req, res, next) => {
   console.log("In admin getProducts middleware");
+
   Product.fetchAll((products) => {
     // Path and file extension is defined in the app.js
     res.render("admin/products", {
