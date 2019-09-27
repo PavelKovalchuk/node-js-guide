16f598af23c78530eb46f9c5a309f2ae95e619e6
PavelKovalchuk <pavel.kovalchuk@dataart.com>
2019-09-27 17:06:37 +0300

    193/ update quantity in cart

--- models/user.js
+++ models/user.js
@@ -27,11 +27,22 @@ class User {
   }
 
   addToCart(product) {
-    // const cartProduct = this.cart.items.findIndex((item) => {
-    //   return item._id === product._id;
-    // });
+    console.log("this.cart", this.cart);
+    const cartProductIndex = this.cart.items.findIndex((item) => {
+      return item.productId.toString() === product._id.toString();
+    });
 
-    const updatedCart = [{items: {productId: new ObjectId(product._id), quantity: 1}}];
+    let newQuantity = 1;
+    const updatedCartItems = [...this.cart.items];
+
+    if (cartProductIndex >= 0) {
+      newQuantity = this.cart.items[cartProductIndex].quantity + 1;
+      updatedCartItems[cartProductIndex].quantity = newQuantity;
+    } else {
+      updatedCartItems.push({productId: new ObjectId(product._id), quantity: newQuantity});
+    }
+
+    const updatedCart = {items: updatedCartItems};
     const db = getDb();
     return db
       .collection("users")
